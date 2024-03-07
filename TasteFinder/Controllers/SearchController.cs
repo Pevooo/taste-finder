67e9cc2b73c5b1e18dec77450535094b9aa8bd6a
Pevooo <pavlysamuelatef@gmail.com>
2024-03-07 21:23:06 +0200

    Imlement Feature Filter and Simple Search

---
+++ TasteFinder/Controllers/SearchController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TasteFinder.Models;
+
+namespace TasteFinder.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly TasteFinderContext db;
+        public SearchController(TasteFinderContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [NonAction]
+        public List<Restaurant> GetResults(List<bool> filters)
+        {
+            List<Restaurant> restaurants = db.Restaurants.ToList();
+            List<Restaurant> results = new();
+
+            foreach (var restaurant in restaurants)
+            {
+                if (filters[0] && !restaurant.OpenAir) continue;
+                if (filters[1] && !restaurant.KidsArea) continue;
+                if (filters[2] && !restaurant.Seats) continue;
+                if (filters[3] && !restaurant.Food) continue;
+                if (filters[4] && !restaurant.Drinks) continue;
+                if (filters[5] && !restaurant.Desserts) continue;
+                if (filters[6] && !restaurant.Delivery) continue;
+                if (filters[7] && !restaurant.Buffet) continue;
+                if (filters[8] && !restaurant.Vegan) continue;
+                if (filters[9] && !restaurant.Healthy) continue;
+
+                results.Add(restaurant);
+            }
+
+            return results;
+        }
+
+
+        [NonAction]
+        public List<Restaurant> GetResults(string searchKey)
+        {
+            
+            List<Restaurant> restaurants = db.Restaurants.ToList();
+
+            List<KeyValuePair<double, Restaurant>> scores = new();
+            searchKey = searchKey.ToLower();
+            foreach (var restaurant in restaurants)
+            {   
+
+                double score = 0;
+                string name = restaurant.Name.ToLower();
+
+                foreach (var character in searchKey)
+                {
+                    if (name.Contains(character))
+                    {
+                        score++;
+                    }
+                }
+
+                scores.Add(new(score, restaurant));
+            }
+
+            scores.Sort(Compare);
+            List<Restaurant> results = scores.Select(p => p.Value).Reverse().ToList();
+            return results;
+        }
+
+        [NonAction]
+        static int Compare(KeyValuePair<double, Restaurant> a, KeyValuePair<double, Restaurant> b)
+        {
+            return b.Key.CompareTo(a.Key);
+        }
+    }
+}
--- TasteFinder/obj/Debug/net6.0/TasteFinder.AssemblyInfo.cs
+++ TasteFinder/obj/Debug/net6.0/TasteFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TasteFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b18808350e44d2c7afc9a54b27d47fd8e57d88ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae228d4ac6a0b5cb2fc8968db7ba2f160c161a50")]
 [assembly: System.Reflection.AssemblyProductAttribute("TasteFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TasteFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
